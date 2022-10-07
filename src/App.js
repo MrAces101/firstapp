6027a6367e602febdaeac608cd0840f5fcbee326
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-10-06 23:45:36 -0400

    Components added

--- src/App.js
+++ src/App.js
@@ -37,12 +37,28 @@ import Promo from "./components/Promo";
 //     );
 // };
 
+
+// function App() {
+//   return (
+//       <div className="App">
+//           <Promo heading="Don't miss this deal!" />
+//           <Promo promoSubHeading="Subscribe to my newsletter and get all the items at 50% off!" />
+//       </div>
+//   );
+// }
+
+
+import Card from './components/Card'
+
 function App() {
   return (
-      <div className="App">
-          <Promo heading="Don't miss this deal!" />
-          <Promo promoSubHeading="Subscribe to my newsletter and get all the items at 50% off!" />
-      </div>
+    <div className="App">
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2" h3="First card's h3"/>
+      <Card h2="Second card's h2" h3="Second card's h3"/>
+      <Card h2="Third card's h2" h3="Third card's h3"/>
+    </div>
   );
-}
+};
+
 export default App;
