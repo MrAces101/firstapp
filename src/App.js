3d1c42023e82387b6fc19bc89a465f5c792df659
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-10-08 14:52:57 -0400

    fruits counter added

--- src/App.js
+++ src/App.js
@@ -63,10 +63,54 @@ import Card from './components/Card'
 
 // export default App;
 
-import Btn from './components/Btn'
+// import Btn from './components/Btn'
+
+// function App() {
+//   return <Btn />
+
+// };
+
+// export default App;
+
+// random number guesing game
+// function App() {
+//   function handleClick() {
+//     let randomNum = Math.floor(Math.random() * 3) + 1;
+//     console.log(randomNum);
+//     let userInput = prompt('type a number');
+//     alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+//   }
+//   return (
+//     <div>
+//       <h1>Task: Add a button and handle a click event</h1>
+//       <button onClick={handleClick}>
+//       Guess the number between 1 and 3    
+//     </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Frutis Scanner - managing state task
+import React from "react";
+import Fruits from "./components/Fruits";
+import FruitsCounter from "./components/FruitsCounter";
 
 function App() {
-  return <Btn />
-};
+  const [fruits] = React.useState([
+        {fruitName: 'apple', id: 1},
+        {fruitName: 'apple', id: 2},
+        {fruitName: 'plum', id: 3},
+  ]);
+  
+  return (
+    <div className="App">
+      <h1>Where should the state go?</h1>
+      <Fruits fruits={fruits} />
+      <FruitsCounter fruits={fruits} />
+    </div>
+  );
+}
 
 export default App;
