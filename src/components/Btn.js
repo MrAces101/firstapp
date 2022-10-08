4fc16968683b1def6cd449a4fc2489d41f0aa26a
MrAces101 <108526744+MrAces101@users.noreply.github.com>
2022-10-08 08:30:20 -0400

    Btn js added

---
+++ src/components/Btn.js
@@ -0,0 +1,23 @@
+// function Btn() {
+//     const clickHandler =
+//     () => console.log('clicked')
+//     return (
+//         <button onClick={'clickHandler'}>
+//             Click Me
+//         </button>
+//     );
+// };
+
+// export default Btn;
+
+// function Btn() {
+//     const hoverHandler =
+//     () => console.log('Mouse Over')
+//     return (
+//         <button onMouseOver={hoverHandler}>
+//             Hover over Me
+//         </button>
+//     );
+// };
+
+// export default Btn;
